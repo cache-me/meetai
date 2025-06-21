6ed9c4963cf6e5aed0c3a632d7deef1f6136009a
cache-me <swainrakeshkumar60@gmail.com>
2025-06-21 16:29:33 +0530

    feat: add useToast hook for managing toast notifications

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { AxiosError } from "axios";
+import { TRPCClientError } from "@trpc/client";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getErrorMessage(
+  error: unknown,
+  defaultMessage: string = "Something went wrong. Please try again."
+) {
+  let message = defaultMessage;
+
+  if (error instanceof AxiosError) {
+    message = error.response?.data?.message;
+  } else if (error instanceof TRPCClientError) {
+    message = error.message;
+  } else if (error instanceof Error) {
+    message = error.message;
+  }
+
+  return message;
 }
